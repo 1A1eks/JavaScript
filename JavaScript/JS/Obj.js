4fc522f015d9dfb07b5cb85c7f8e75362f81a207
1A1eks <aleksander.cornette@gmail.com>
2018-10-03 16:29:13 +0200

    starting stuff

--- JavaScript/JS/Obj.js
+++ JavaScript/JS/Obj.js
@@ -7,10 +7,46 @@ var item = {
     "str" = 0,
     "magic" = 0,
     "minlvl" = 0,
+    "price" = 0,
     "available" = false
 };
 
+const generateName = () => {
+    let name = "";
+    let length = 4+Math.ceil(Math.random()*6);
+    for (let i=0;i<length;i++) {
+        name += String.fromCharCode("a".charCodeAt(0) + Math.floor(Math.random()*27))
+    }
+    return(name);
+};
+
+const basicStatGen = () => {
+    return(Math.ceil()*9);
+};
+
+const createItem = () => {
+    new Item {generateName(), (Math.ceil()*9), (Math.ceil()*9), (Math.ceil()*9), (Math.ceil()*99), true}
+};
+
 var shop = [];
+
+const initiateShop = () => {
+    if (shop.length<2){
+        for(i=0;i<5;i++){
+            shop.push(createItem());
+        }
+    } else {console.log("Shop already initiated!!!")}
+};
+
+const fillShop = () => {
+    let j = 0
+    for ( i ; i<shop.length; i++){
+        if(shop[i]===undefined){
+            shop[i] = createItem();
+        }
+    }
+    if (shop[i])
+};
 
 var player = {
     "name" = "",
