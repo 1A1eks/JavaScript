a73a2bf6453021a9983a289159c1a88935f33898
1A1eks <aleksander.cornette@gmail.com>
2018-10-24 11:41:23 +0200

    overlay, first steps

--- JS/main.js
+++ JS/main.js
@@ -307,12 +307,56 @@ function toggleBattleInstanceOff () {
     
 }
 
+
 function introStart () {
     if (initiated === false) {
+
+        var viewportWidth = window.innerWidth;
+        var viewportHeight = window.innerHeight;
+        //BIG-SCREENS
+        if (viewportWidth > 1080) {
+        var batText = document.getElementsByClassName('battleText')[0];
         var introSections = document.getElementsByClassName("intro");
         var overlayBackground = document.createElement('div');
         document.body.insertAdjacentElement("afterbegin", overlayBackground);
 
+        overlayBackground.style.position = "fixed";
+        overlayBackground.style.top = "0";
+        overlayBackground.style.left = "0";
+        overlayBackground.style.backgroundColor = "#2a2929";
+        overlayBackground.style.width = "100%";
+        overlayBackground.style.height = "100vh";
+        overlayBackground.style.overflow = "scroll";
+
+        console.log("ok2" + initiated);
+        introSections[0].style.position ="fixed";
+        introSections[0].style.zIndex = "99";
+        introSections[0].style.width = "50%";
+        introSections[0].style.top = "0%";
+        introSections[0].style.marginLeft = "-500px";
+        introSections[0].style.left = "50%";
+
+        introSections[1].style.position ="fixed";
+        introSections[1].style.zIndex = "99";
+        introSections[1].style.width = "50%";
+        introSections[1].style.marginLeft = "350px";
+        introSections[1].style.top = "0%";
+        introSections[1].style.left = "5%";
+
+        introSections[2].style.position ="fixed";
+        introSections[2].style.zIndex = "99";
+        introSections[2].style.top = "10%";
+        introSections[2].style.textAlign = "center";
+        introSections[2].style.left = "50%";
+        introSections[2].style.marginLeft = "-325px";
+
+        batText.style.border = "none";
+        //MEDIUM-SCREENS    
+        } else if (viewportWidth > 720) {
+        var batText = document.getElementsByClassName('battleText')[0];
+        var introSections = document.getElementsByClassName("intro");
+        var overlayBackground = document.createElement('div');
+        document.body.insertAdjacentElement("afterbegin", overlayBackground);
 
         overlayBackground.style.position = "fixed";
         overlayBackground.style.top = "0";
@@ -342,12 +386,14 @@ function introStart () {
         introSections[2].style.top = "50%";
         introSections[2].style.textAlign = "center";
         introSections[2].style.left = "50%";
-        introSections[2].style.marginLeft = "-325px";}
+        introSections[2].style.marginLeft = "-325px";
 
-        
+        batText.style.border = "none";
+        //MOBILE-VERSIONS
+        } else {
 
-
-    
+        }
+    }    
 }
 //============-Gameplay-Combat-=================
 
@@ -493,7 +539,7 @@ function adjustBattleText(message1) {
 
    document.getElementsByTagName('p')[document.getElementsByTagName('p').length-1].innerHTML 
    = message1;
-   document.getElementById(`p${count}`).scrollIntoView();
+   //document.getElementById(`p${count}`).scrollIntoView();
    return;
     }
 
@@ -506,7 +552,7 @@ function adjustBattleText(message1) {
         document.getElementsByTagName('p')[i].innerHTML = message1;
         message1 = temporary;
     }
-    document.getElementById(`p${count}`).scrollIntoView();
+    //document.getElementById(`p${count}`).scrollIntoView();
     return;
 }
 
